14372761d9d358ebf54b38f7ab724d059c31cd5a
Holamite <olamideadebara@gmail.com>
2024-02-26 21:53:03 +0100

    Successfull deployed

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
@@ -17,16 +17,13 @@ contract NFTMarket is ERC721, Ownable {
     function safeMint() public {
         uint256 tokenId = _nextTokenId++;
         _safeMint(msg.sender, tokenId);
-        _setTokenURI(tokenId, getTokenURI());
     }
 
     function setTokenPrice(uint256 tokenId, uint256 price) public {
-        require(_exists(tokenId), "Token does not exist");
         _tokenPrices[tokenId] = price;
     }
 
-    function purchaseNFT(uint tokenId) returns () {
-        require(_exists(tokenId), "Token does not exist");
+    function purchaseNFT(uint tokenId) public payable {
         require(msg.value >= _tokenPrices[tokenId], "Insufficient payment");
 
         address owner = ownerOf(tokenId);
@@ -37,17 +34,6 @@ contract NFTMarket is ERC721, Ownable {
     }
 
     function getTokenPrice(uint256 tokenId) public view returns (uint256) {
-        require(_exists(tokenId), "Token does not exist");
         return _tokenPrices[tokenId];
-    }
-
-    function getTokenURI(uint256 tokenId) public pure returns (string memory) {
-        return
-            string(
-                abi.encodePacked(
-                    "data:application/json;base64,",
-                    Base64.encode(tokenId)
-                )
-            );
     }
 }
