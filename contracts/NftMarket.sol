e1259c211aaf9b948e6e24797ea376c833e56e95
Holamite <olamideadebara@gmail.com>
2024-02-26 18:51:52 +0100

    completed the contract

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -10,6 +10,7 @@ contract NFTMarket is ERC721, Ownable {
     using Strings for uint256;
 
     uint256 private _nextTokenId;
+    mapping(uint256 => uint256) private _tokenPrices;
 
     constructor() ERC721("Holamite", "HOT") Ownable(msg.sender) {}
 
@@ -19,5 +20,34 @@ contract NFTMarket is ERC721, Ownable {
         _setTokenURI(tokenId, getTokenURI());
     }
 
-    function purchaseNFT(uint tokenId, uint _amount) returns () {}
+    function setTokenPrice(uint256 tokenId, uint256 price) public {
+        require(_exists(tokenId), "Token does not exist");
+        _tokenPrices[tokenId] = price;
+    }
+
+    function purchaseNFT(uint tokenId) returns () {
+        require(_exists(tokenId), "Token does not exist");
+        require(msg.value >= _tokenPrices[tokenId], "Insufficient payment");
+
+        address owner = ownerOf(tokenId);
+        _transfer(owner, msg.sender, tokenId);
+
+        // Send payment to the seller
+        payable(owner).transfer(msg.value);
+    }
+
+    function getTokenPrice(uint256 tokenId) public view returns (uint256) {
+        require(_exists(tokenId), "Token does not exist");
+        return _tokenPrices[tokenId];
+    }
+
+    function getTokenURI(uint256 tokenId) public pure returns (string memory) {
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(tokenId)
+                )
+            );
+    }
 }
